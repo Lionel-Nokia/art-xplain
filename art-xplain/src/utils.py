21dae9eea6dbca54894d1365300d90ac9774c458
Lionel Natarianni <lionel.natarianni@nokia-bell-labs.com>
2026-03-18 10:54:36 +0100

    LN - fix config path in utils.py

--- art-xplain/src/utils.py
+++ art-xplain/src/utils.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from pathlib import Path
 import yaml
 
-def load_config(config_path: str | Path = "../config/config.yaml") -> dict:
+def load_config(config_path: str | Path = "config/config.yaml") -> dict:
 #def load_config(config_path: str | Path = "config.yaml") -> dict:
     config_path = Path(config_path)
     if not config_path.exists():
